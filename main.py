f31b1305ea7dacb53ec59d2c394240a15cb9b98d
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2025-02-01 14:03:25 -0800

    added map loading and drawing

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
+from tilemap import load_map
 
 
 def game() -> None:
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
+
+    tilemap = load_map("assets/maps/map1.json")
+    tileset_img = pygame.image.load("assets/tilesets/TilesetFloor.png")
 
     running = True
     while running:
@@ -13,6 +17,9 @@ def game() -> None:
                 running = False
 
         screen.fill((120, 180, 255, 255))
+
+        tilemap.draw(screen, tileset_img)
+
         pygame.display.update()
 
     pygame.quit()
