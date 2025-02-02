de9cd0870795f8c3b91669d31fb8444d3b644a4c
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2025-02-01 22:19:29 -0800

    added animations and images for projectiles

--- Enemy.py
+++ Enemy.py
@@ -4,44 +4,46 @@ from pygame.math import Vector2
 from constants import Constants
 import random
 
-def random_spawn_location()-> tuple[int, int]:
-    location = random.randint(1,4)
+
+def random_spawn_location() -> tuple[int, int]:
+    location = random.randint(1, 4)
     if location == 1:
-        #top
+        # top
         return (5, 0)
     elif location == 2:
-        #right
+        # right
         return (5, 5)
     elif location == 3:
-        #bottom
+        # bottom
         return (5, 5)
     elif location == 4:
-        #left
+        # left
         return (5, 5)
-
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x: int, 
-                y: int, 
-                width: int, 
-                height: int, 
-                direction: Vector2 = Vector2(0, 1), 
-                speed: int = 1,
-                health: int = 10,
-                init_pos: tuple[int, int] = (5, 5)
-                ):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        direction: Vector2 = Vector2(0, 1),
+        speed: int = 200,
+        health: int = 10,
+        init_pos: tuple[int, int] = (5, 5),
+    ):
         super().__init__()
-        
+
         self.srect = Rect(0, 0, 16, 16)
-        self.direction = direction 
+        self.direction = direction
         self.vel = Vector2(0, 0)
-        self.speed = speed 
+        self.speed = speed
         self.max_health = health
         self.curr_health = health
         init_pos = random_spawn_location()
         self.drect = Rect(
-            # 
+            #
             init_pos[0] * Constants.TILESIZE,
             init_pos[1] * Constants.TILESIZE,
             Constants.TILESIZE,
@@ -63,14 +65,13 @@ class Enemy(pygame.sprite.Sprite):
         self.vel.x = 0
         self.vel.y = 0
 
-
     def destroy(self):
         print("Enemy destroyed")
 
     def move_x(self, dt: float) -> None:
         self.drect.x += self.vel.x * dt
         self.hitbox.x += self.vel.x * dt
-        
+
     def move_y(self, dt: float) -> None:
         self.drect.y += self.vel.y * dt
         self.hitbox.y += self.vel.y * dt
@@ -87,11 +88,10 @@ class Enemy(pygame.sprite.Sprite):
 class Zombie(Enemy):
     def __init__(self, x, y, width, height, speed=1, health=15):
         super().__init__(x, y, width, height, Vector2(0, 1), speed, health)
-        self.attack_power = 5 
+        self.attack_power = 5
         self.sprite_sheet = pygame.image.load(
             "assets/characters/NinjaDark/SpriteSheet.png"
         )
-
 
     def load_sprite(self, sprite_path: str) -> None:
         self.sprite_sheet = pygame.image.load(sprite_path)
@@ -99,3 +99,4 @@ class Zombie(Enemy):
     def follow_player(self, player_rect: Rect, dt: float) -> None:
         self.vel.x = self.speed  # Move right at constant speed
         self.vel.y = 0  # No vertical movement
+
--- projectile.py
+++ projectile.py
@@ -2,13 +2,21 @@ import pygame
 from pygame import Rect
 from pygame.math import Vector2
 from constants import Constants
+from animation import Animation
 
 
 class Projectile:
 
     lifespan = 1
 
-    def __init__(self, pos: tuple[int, int], dir: Vector2, speed: float):
+    def __init__(
+        self,
+        pos: tuple[int, int],
+        dir: Vector2,
+        speed: float,
+        img: pygame.Surface,
+        anim: Animation,
+    ):
         self.drect = Rect(
             pos[0],
             pos[1],
@@ -19,8 +27,12 @@ class Projectile:
         self.speed = speed
         self.lifespan = Projectile.lifespan
         self.alive = True
+        self.img = img
+        self.img_num_tiles_per_row = int(self.img.width / Constants.TILESIZE)
+        self.anim = anim
 
     def update(self, dt: float) -> None:
+        self.anim.update(dt)
         self.lifespan -= dt
         if self.lifespan < 0.0:
             self.lifespan = 0.0
@@ -33,9 +45,15 @@ class Projectile:
         self.drect.y += self.dir.y * self.speed * dt
 
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.rect(
-            screen,
-            (255, 0, 0),
+        # DEBUG RECT BELOW
+        # pygame.draw.rect(
+        #     screen,
+        #     (255, 0, 0),
+        #     self.drect,
+        #     1,
+        # )
+        screen.blit(
+            self.img,
             self.drect,
-            1,
+            self.anim.frame(self.img_num_tiles_per_row, Constants.TILESIZE),
         )
