21a1e5bb62e2ce738d37a72f396b921ae7a9dfd0
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2025-02-01 16:40:23 -0800

    added player collision code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import Constants
 from tilemap import load_map
 from utils import get_hitboxes
+from player import Player
 
 
 def game() -> None:
@@ -19,12 +20,40 @@ def game() -> None:
 
     clock = pygame.time.Clock()
 
+    player = Player([], (0, 0))
+
     running = True
+    dt = 0.0
     while running:
 
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = False
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_q:
+                    running = False
+
+        player.update()
+
+        player.move_x(dt)
+
+        for rect in tree_rects:
+            if player.hitbox.colliderect(rect):
+                if player.vel.x > 0:
+                    player.hitbox.right = rect.left
+                else:
+                    player.hitbox.left = rect.right
+                player.update_drect_from_hitbox()
+
+        player.move_y(dt)
+
+        for rect in tree_rects:
+            if player.hitbox.colliderect(rect):
+                if player.vel.y > 0:
+                    player.hitbox.bottom = rect.top
+                else:
+                    player.hitbox.top = rect.bottom
+                player.update_drect_from_hitbox()
 
         screen.fill((120, 180, 255, 255))
 
@@ -36,6 +65,7 @@ def game() -> None:
             sprite_size=(Constants.TILESIZE * 2, Constants.TILESIZE * 2),
             tileset_width_in_tiles=12,
         )
+        player.draw(screen)
 
         for rect in tree_rects:
             pygame.draw.rect(
@@ -45,11 +75,24 @@ def game() -> None:
                 1,
             )
 
+        pygame.draw.rect(
+            screen,
+            (0, 255, 0),
+            player.hitbox,
+            1,
+        )
+        pygame.draw.rect(
+            screen,
+            (0, 0, 255),
+            player.drect,
+            1,
+        )
+
         # screen.blit(player_img, pos, pygame.rect.Rect(0, 0, 16, 16))
 
         pygame.display.update()
         # print("tick " + str(pygame.time.get_ticks()))
-        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
         # foo = 1
         # print(f"foo is {foo}", )
 
--- player.py
+++ player.py
@@ -1,19 +1,73 @@
 import pygame
+from pygame.rect import Rect
+from pygame.math import Vector2
 from constants import Constants
 
 
 class Player(pygame.sprite.Sprite):
+
+    speed = 200
+
     def __init__(self, groups, init_pos: tuple[int, int]):
-        self.drect = pygame.rect.Rect(
+        self.sprite_sheet = pygame.image.load(
+            "assets/characters/NinjaDark/SpriteSheet.png"
+        )
+        self.srect = Rect(
+            0,
+            0,
+            16,
+            16,
+        )
+        self.drect: Rect = Rect(
             init_pos[0] * Constants.TILESIZE,
             init_pos[1] * Constants.TILESIZE,
             Constants.TILESIZE,
             Constants.TILESIZE,
         )
-        self.hitbox = self.drect.copy()
+        self.hitbox_topleft_offset = (4, 4)
+        self.vel = Vector2(0, 0)
+        self.hitbox: Rect = Rect(
+            init_pos[0] * Constants.TILESIZE + self.hitbox_topleft_offset[0],
+            init_pos[1] * Constants.TILESIZE + self.hitbox_topleft_offset[1],
+            Constants.TILESIZE - self.hitbox_topleft_offset[0] * 2,
+            Constants.TILESIZE - self.hitbox_topleft_offset[1] * 2,
+        )
 
     def update(self) -> None:
-        pass
+        self.vel.x = 0
+        self.vel.y = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.vel.y = -Player.speed
+        if keys[pygame.K_a]:
+            self.vel.x = -Player.speed
+        if keys[pygame.K_s]:
+            self.vel.y = Player.speed
+        if keys[pygame.K_d]:
+            self.vel.x = Player.speed
 
     def draw(self, screen: pygame.Surface) -> None:
-        pass
+        screen.blit(
+            self.sprite_sheet,
+            self.drect,
+            self.srect,
+        )
+
+    def collides(self, rect: Rect) -> bool:
+        return self.hitbox.colliderect(rect)
+
+    def move_x(self, dt: float) -> None:
+        self.drect.x += self.vel.x * dt
+        self.hitbox.x += self.vel.x * dt
+
+    def move_y(self, dt: float) -> None:
+        self.drect.y += self.vel.y * dt
+        self.hitbox.y += self.vel.y * dt
+
+    def update_drect_from_hitbox(self) -> None:
+        self.drect = Rect(
+            self.hitbox.x - self.hitbox_topleft_offset[0],
+            self.hitbox.y - self.hitbox_topleft_offset[1],
+            Constants.TILESIZE,
+            Constants.TILESIZE,
+        )
